292c7661b82c60f30587d84111b337bb824dca10
Aman Kumar <amankarguwal0@gmail.com>
2024-06-08 23:03:15 +0530

    Create 02_dataTypes.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello"); // we are using Node.js, not browser
+
+// console.log(7
+//      +3
+//     +6); // code readability should be high / maintainable
+
+console.log("Aman Kumar");
+
+console.log(7 + 3 + 6); // code readability should be high / maintainable
+
+let name = "aman";
+let age = 21;
+let isMale = true;
+let state = null;
+
+// number => 2 to power 53 - 1
+// BigInt => 2 to power 53 - 1 and above
+// string => " " or ' ' or ` `
+// boolean => true or false
+// null => null // standalone value | absence of value | nothing
+// undefined => undefined // standalone value | absence of value | nothing
+// symbol => unique value
+
+// object => key-value pairs
+
+console.log(typeof name); // string
+console.log(typeof age); // number
+console.log(typeof isMale); // boolean
+console.log(typeof state); // object
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
