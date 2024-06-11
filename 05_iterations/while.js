efdd3ec941cba768e43afd24f9890905836c36e3
Aman Kumar <amankarguwal0@gmail.com>
2024-06-11 23:23:43 +0530

    Create while.js

---
+++ 05_iterations/while.js
@@ -0,0 +1,22 @@
+// while
+
+let index = 0;
+while(index <= 10){
+    // console.log(`value of index: ${index}`);
+    index+=2;
+}
+
+let myArray = ["flash" , "batman" , "superman" ];
+
+let index2 = 0;
+while(index2 < myArray.length){
+    const element = myArray[index2];
+    // console.log(`Value of element: ${element}`);
+    index2++;
+}
+
+let score = 5;
+do{
+    // console.log(`Value of score: ${score}`);
+    score++;
+}while (score <= 10);
