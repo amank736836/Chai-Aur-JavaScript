03bde9231ed2bf23a5896c492358cb5b0f4a0ef7
Aman Kumar <amankarguwal0@gmail.com>
2024-06-11 15:37:29 +0530

    Create switch.js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,99 @@
+// switch
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3;
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("August");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("October");
+        break;
+    case 11:
+        console.log("November");
+        break;
+    case 12:
+        console.log("December");
+        break;
+    default:
+        console.log("Invalid month");
+        break;
+}
+
+const month2 = "March";
+
+switch (month2) {
+    case "January":
+        console.log("1");
+        break;
+    case "February":
+        console.log("2");
+        break;
+    case "March":
+        console.log("3");
+        break;
+    case "April":
+        console.log("4");
+        break;
+    case "May":
+        console.log("5");
+        break;
+    case "June":
+        console.log("6");
+        break;
+    case "July":
+        console.log("7");
+        break;
+    case "August":
+        console.log("8");
+        break;
+    case "September":
+        console.log("9");
+        break;
+    case "October":
+        console.log("10");
+        break;
+    case "November":
+        console.log("11");
+        break;
+    case "December":
+        console.log("12");
+        break;
+    default:
+        console.log("Invalid month");
+        break;
+}
+
