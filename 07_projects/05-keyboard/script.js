db61e6f8ee902b8ec4c0cc99f8d656d42b9789f2
Aman Kumar <amankarguwal0@gmail.com>
2024-06-13 23:23:05 +0530

    Update script.js

--- 07_projects/05-keyboard/script.js
+++ 07_projects/05-keyboard/script.js
@@ -1,1 +1,21 @@
-console.log('Project 5');
+// console.log('Project 5');
+
+const insert = document.getElementById('insert');
+window.addEventListener('keydown', (e) => {
+    insert.innerHTML = `
+        <div class="color">
+            <table>
+                <tr>
+                    <td>Event Key</td>
+                    <td>Event KeyCode</td>
+                    <td>Event Code</td>
+                </tr>
+                <tr>
+                    <td>${e.key === ' ' ? 'Space' : e.key}</td>
+                    <td>${e.keyCode}</td>
+                    <td>${e.code}</td>
+                </tr>
+            </table>
+        </div>
+    `;
+});
